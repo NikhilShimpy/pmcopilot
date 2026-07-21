29ec1a66b61c8bfa48853414cfc71e2d38a55ca5
Nikhil Shimpy <nikhilshimpy712@gmail.com>
2026-07-21 23:44:13 +0530

    Define ExtractedContext interface for input analysis
    Added ExtractedContext interface to define structure for input analysis.

--- lib/fallbackAnalysis.ts
+++ lib/fallbackAnalysis.ts
@@ -16,7 +16,19 @@ import { logger } from './logger'
 // ============================================
 // INPUT ANALYSIS HELPERS
 // ============================================
-
+interface ExtractedContext {
+  productType: string;
+  targetUsers: string;
+  primaryFeatures: string[];
+  industry: string;
+  keywords: string[];
+  isApp: boolean;
+  isSaaS: boolean;
+  isB2B: boolean;
+  isB2C: boolean;
+  hasAI: boolean;
+  hasMobile: boolean;
+}
 
 
 function extractContextFromInput(feedback: string): ExtractedContext {
